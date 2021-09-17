c01ab5014cc54f2260eff573dff4bda2d00c9c98
Baklaikin Kostiantyn <baklaikin@gmail.com>
2021-09-17 22:39:15 +0300

    Phoneform done

--- src/components/PhoneForm/PhoneForm.jsx
+++ src/components/PhoneForm/PhoneForm.jsx
@@ -1,65 +1,70 @@
-import React, { Component } from "react";
+import { useState } from "react";
 import s from "components/PhoneForm/PhoneForm.module.css";
 
-class PhoneForm extends Component {
-  state = {
-    name: "",
-    number: "",
-  };
+function PhoneForm({ onSubmit }) {
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
 
-  handleInputChange = (e) => {
+  const handleInputChange = (e) => {
     const data = e.currentTarget.value;
-    this.setState({ [e.currentTarget.name]: data });
+    switch (e.currentTarget.name) {
+      case "name":
+        setName(data);
+        break;
+      case "number":
+        setNumber(data);
+        break;
+      default:
+        return;
+    }
   };
 
-  addToContacts = (e) => {
+  const addToContacts = (e) => {
     e.preventDefault();
-    this.props.onSubmit(this.state);
-    this.resetForm();
+    const data = { name, number };
+    console.log(data);
+    onSubmit(data);
+    resetForm();
   };
 
-  resetForm = () => {
-    this.setState({
-      name: "",
-      number: "",
-    });
+  const resetForm = () => {
+    setName("");
+    setNumber("");
   };
 
-  render() {
-    return (
-      <form onSubmit={this.addToContacts} className={s.form}>
-        <label className={s.formLabel}>
-          Name
-          <input
-            className={s.formInput}
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-            required
-            value={this.state.name}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <label className={s.formLabel}>
-          Number
-          <input
-            className={s.formInput}
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Номер телефона должен состоять из цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
-            required
-            value={this.state.number}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <button className={s.addToContacts} type="submit">
-          Add to contacts
-        </button>
-      </form>
-    );
-  }
+  return (
+    <form onSubmit={addToContacts} className={s.form}>
+      <label className={s.formLabel}>
+        Name
+        <input
+          className={s.formInput}
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+          required
+          value={name}
+          onChange={handleInputChange}
+        />
+      </label>
+      <label className={s.formLabel}>
+        Number
+        <input
+          className={s.formInput}
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Номер телефона должен состоять из цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+          required
+          value={number}
+          onChange={handleInputChange}
+        />
+      </label>
+      <button className={s.addToContacts} type="submit">
+        Add to contacts
+      </button>
+    </form>
+  );
 }
 
 export default PhoneForm;
